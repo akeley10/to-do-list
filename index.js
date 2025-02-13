d671435f97c58785a8353a99f6371256401e5f42
Alejandro Martel <91066025+akeley10@users.noreply.github.com>
2025-02-13 11:03:29 +0000

    separando y ordenando el javascript en otro archivo

---
+++ index.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    const formulario = document.getElementById('lista');
+    const agregarBtn = document.getElementById('agregar');
+    const eliminarBtn = document.getElementById('eliminar');
+    const resultado = document.getElementById('resultadoLista');
+    const arrayDatos = [];
+
+
+    formulario.addEventListener("submit",(e=>{
+       e.preventDefault();
+       const valorList = document.getElementById('inputLista').value;
+       arrayDatos.push(valorList);
+       let resultadoHtml= "";
+       for (let i = 0; i < arrayDatos.length; i++) {
+        resultadoHtml += '<li>'+ arrayDatos[i] + '</li>'+'<br>';
+        
+       }
+       resultado.innerHTML = resultadoHtml;
+
+    }));
+
+    eliminarBtn.addEventListener("click",(e)=>{
+        e.preventDefault();
+        arrayDatos.pop();
+        resultado.innerHTML = arrayDatos;
+    });
+
+});

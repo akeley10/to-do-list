ed6a5f725ace34dd31a005265833f7abb981354c
Alejandro Martel <91066025+akeley10@users.noreply.github.com>
2025-02-14 12:44:25 +0000

    guardando datos en cookies

--- index.js
+++ index.js
@@ -5,18 +5,22 @@ document.addEventListener('DOMContentLoaded',()=>{
     const resultado = document.getElementById('resultadoLista');
     const arrayDatos = [];
 
+    
+
 
     formulario.addEventListener("submit",(e=>{
        e.preventDefault();
        const valorList = document.getElementById('inputLista').value;
        arrayDatos.push(valorList);
+       document.cookie = "resultado=" + JSON.stringify(arrayDatos) +";"+ "expires=Thu, 18 Dec 2025 12:00:00 UTC; path=/;";
        let resultadoHtml= "";
        for (let i = 0; i < arrayDatos.length; i++) {
         resultadoHtml += '<li>'+ arrayDatos[i] + '</li>'+'<br>';
         
        }
-     
+
        resultado.innerHTML = resultadoHtml;
+       console.log( document.cookie );
        formulario.reset();
 
     }));
